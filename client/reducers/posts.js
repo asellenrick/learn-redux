c8b34bb359fa19973f790b7b7f15a34d81987ae7
Andrew Sellenrick <andrewsellenrick@nanonation.net>
2018-09-19 05:52:11 -0500

    Finished handling the first action, for likes.

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,4 +1,10 @@
 export default function posts(state = [], action) {
-  console.log("Posts Reducer:", action);
-  return state;
+	switch (action.type) {
+		case 'INCREMENT_LIKES':
+			const i = action.index;
+			return [...state.slice(0, i), { ...state[i], likes: state[i].likes + 1 }, ...state.slice(i + 1)];
+			break;
+		default:
+			return state;
+	}
 }
