08002edee07e8f58eb5bdc0faf0e63324742f86c
Andrew Sellenrick <andrewsellenrick@nanonation.net>
2018-09-22 08:21:00 -0500

    Added post actions, hooked up hot reload for reducers, and connected out store to the redux dev tools.

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
-  render() {
-    return <div className="single-photo">Single</div>;
-  }
+	render() {
+		const id = this.props.params.postId;
+		const i = this.props.posts.findIndex(post => post.code == id);
+		const post = this.props.posts[i];
+		const postComments = this.props.comments[id] || [];
+		return (
+			<div className="single-photo">
+				<Photo i={i} post={post} {...this.props} />
+				<Comments postComments={postComments} {...this.props} />
+			</div>
+		);
+	}
 });
 
 export default Single;
--- client/store.js
+++ client/store.js
@@ -1,19 +1,30 @@
-import { createStore, compose } from "redux";
-import { syncHistoryWithStore } from "react-router-redux";
-import { browserHistory } from "react-router";
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 
-import rootReducer from "./reducers/index";
+import rootReducer from './reducers/index';
 
-import comments from "./data/comments";
-import posts from "./data/posts";
+import comments from './data/comments';
+import posts from './data/posts';
 
 const defaultState = {
-  posts,
-  comments
+	posts,
+	comments
 };
 
-const store = createStore(rootReducer, defaultState);
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const enhancers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+// Allow hot reloading of reducers
+if (module.hot) {
+	module.hot.accept('./reducers/', () => {
+		const nextRootReducer = require('./reducers/index').default;
+		store.replaceReducer(nextRootReducer);
+	});
+}
 
 export default store;
